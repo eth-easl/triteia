d24becf17d8e1a557c8e6f7ffcafc52c1269972f
Xiaozhe Yao <askxzyao@gmail.com>
2024-06-06 16:46:37 +0200

    wip improve kernel

--- triteia/csrc/marlin/marlin_cuda.cpp
+++ triteia/csrc/marlin/marlin_cuda.cpp
@@ -41,10 +41,19 @@ int marlin_cuda(const void *A, const void *B, void *C, void *s, int prob_m,
                 int dev = 0, cudaStream_t stream = 0, int thread_k = -1,
                 int thread_n = -1, int sms = -1, int max_par = 16);
 
-int marlin_cuda_bmm_2_4(const void *A, const void *B, const void *meta, void *C, void *s,
-  int prob_m, int prob_n, int prob_k, void *workspace, int groupsize=-1,
-  int dev=0, cudaStream_t stream = 0, int thread_k = -1,
-  int thread_m = -1, int sms = -1, int max_par = 16);
+int marlin_cuda_bmm_2_4(const void *A, const void *B, const void *meta, void *C,
+                        void *s, int prob_m, int prob_n, int prob_k,
+                        void *workspace, int groupsize = -1, int dev = 0,
+                        cudaStream_t stream = 0, int thread_k = -1,
+                        int thread_m = -1, int sms = -1, int max_par = 16);
+
+int marlin_cuda_ibmm_2_4(const void *A, const void *B, const void *meta,
+                         void *C, void *s, const void *indices,
+                         const void *starts, const void *counts, int prob_m,
+                         int prob_n, int prob_k, void *workspace,
+                         int groupsize = -1, int dev = 0,
+                         cudaStream_t stream = 0, int thread_k = -1,
+                         int thread_m = -1, int sms = -1, int max_par = 16);
 
 const int ERR_PROB_SHAPE = 1;
 const int ERR_KERN_SHAPE = 2;
@@ -91,7 +100,7 @@ void mul_2_4(const torch::Tensor &A, const torch::Tensor &B,
     AT_ERROR("workspace must be of size at least ", prob_m / 128 * max_par,
              ".");
   int dev = A.get_device();
-  //at::cuda::CUDAStream stream = at::cuda::getStreamFromPool(0, dev);
+  // at::cuda::CUDAStream stream = at::cuda::getStreamFromPool(0, dev);
   at::cuda::CUDAStream stream = at::cuda::getDefaultCUDAStream(dev);
   {
     int err = marlin_cuda_2_4(A.data_ptr(), B.data_ptr(), meta.data_ptr(),
@@ -123,7 +132,6 @@ void mul_stream(const torch::Tensor &A, const torch::Tensor &B,
             meta[indices[i]], sliced_C, s[indices[i]], my_workspace, thread_k,
             thread_n, sms, max_par);
   }
-  
 }
 
 void mul_stream_parallel(const torch::Tensor &A, const torch::Tensor &B,
@@ -137,7 +145,7 @@ void mul_stream_parallel(const torch::Tensor &A, const torch::Tensor &B,
     mul_threads[i] =
         std::thread([i, &A, &B, &meta, &C, &s, &indices, &workspace, &starts,
                      &counts, thread_k, thread_n, sms, max_par]() {
-          int  start = starts[i].item<int>();
+          int start = starts[i].item<int>();
           auto sliced_C = C.slice(0, start, start + counts[i].item<int>());
           auto my_workspace = workspace[i];
           mul_2_4(A.slice(0, start, start + counts[i].item<int>()),
@@ -160,9 +168,9 @@ void bmm_2_4(const torch::Tensor &A, const torch::Tensor &B,
    * C:    [n, m]: n: #reqs, m: out features
    * s:    [n, 1, m]: n: #reqs, m: out features
    * meta: [n, k, m/16]: n: #reqs, k: in features, m: out features
-  */
+   */
 
-  int prob_n = A.size(0); 
+  int prob_n = A.size(0);
   int prob_m = C.size(1);
   int prob_k = A.size(1);
 
@@ -173,10 +181,10 @@ void bmm_2_4(const torch::Tensor &A, const torch::Tensor &B,
     AT_ERROR("workspace must be of size at least ", prob_n / 128 * max_par,
              ".");
   int dev = A.get_device();
-  int err = marlin_cuda_bmm_2_4(A.data_ptr(), B.data_ptr(),meta.data_ptr(), C.data_ptr(), s.data_ptr(),
-                        prob_m, prob_n, prob_k, workspace.data_ptr(), groupsize,
-                        dev, at::cuda::getCurrentCUDAStream(dev), thread_k,
-                        thread_m, sms, max_par);
+  int err = marlin_cuda_bmm_2_4(
+      A.data_ptr(), B.data_ptr(), meta.data_ptr(), C.data_ptr(), s.data_ptr(),
+      prob_m, prob_n, prob_k, workspace.data_ptr(), groupsize, dev,
+      at::cuda::getCurrentCUDAStream(dev), thread_k, thread_m, sms, max_par);
   if (err == ERR_PROB_SHAPE) {
     AT_ERROR("Problem (m=", prob_m, ", n=", prob_n, ", k=", prob_k, ")",
              " not compatible with thread_k=", thread_k,
@@ -187,11 +195,43 @@ void bmm_2_4(const torch::Tensor &A, const torch::Tensor &B,
   }
 }
 
+void ibmm(const torch::Tensor &A, const torch::Tensor &B,
+          const torch::Tensor &meta, torch::Tensor &C, const torch::Tensor &s,
+          const torch::Tensor &indices, torch::Tensor &workspace,
+          const torch::Tensor &starts, const torch::Tensor &counts,
+          int thread_k = -1, int thread_n = -1, int sms = -1, int max_par = 8) {
+  int prob_n = A.size(0);
+  int prob_m = C.size(1);
+  int prob_k = A.size(1);
+
+  int groupsize = (s.size(1) == 1) ? -1 : prob_k / s.size(1);
+  if (groupsize != -1 && groupsize * s.size(1) != prob_k)
+    AT_ERROR("k=", prob_k, " not compatible with ", s.size(0), " groups.");
+  if (workspace.numel() < prob_n / 128 * max_par)
+    AT_ERROR("workspace must be of size at least ", prob_n / 128 * max_par,
+             ".");
+  int dev = A.get_device();
+  int err = marlin_cuda_ibmm_2_4(
+      A.data_ptr(), B.data_ptr(), meta.data_ptr(), C.data_ptr(), s.data_ptr(),
+      indices.data_ptr(), starts.data_ptr(), counts.data_ptr(), prob_m, prob_n,
+      prob_k, workspace.data_ptr(), groupsize, dev,
+      at::cuda::getCurrentCUDAStream(dev), thread_k, thread_n, sms, max_par);
+  if (err == ERR_PROB_SHAPE) {
+    AT_ERROR("Problem (m=", prob_m, ", n=", prob_n, ", k=", prob_k, ")",
+             " not compatible with thread_k=", thread_k,
+             ", thread_n=", thread_n, ".");
+  } else if (err == ERR_KERN_SHAPE) {
+    AT_ERROR("No kernel implementation for thread_k=", thread_k,
+             ", thread_n=", thread_n, ", groupsize=", groupsize, ".");
+  }
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("mul", &mul, "Marlin FP16xINT4 matmul.");
   m.def("mul_2_4", &mul_2_4, "Marlin FP16xINT4 matmul with 2:4 sparsity.");
   m.def("mul_stream", &mul_stream, "Marlin FP16xINT4 matmul with stream.");
-  m.def("mul_stream_parallel", &mul_stream_parallel, "Marlin FP16xINT4 matmul with stream.");
-  m.def("bmm_2_4", &bmm_2_4,"Batched FP16xINT4 matmul with stream.");
+  m.def("mul_stream_parallel", &mul_stream_parallel,
+        "Marlin FP16xINT4 matmul with stream.");
+  m.def("bmm_2_4", &bmm_2_4, "Batched FP16xINT4 matmul with stream.");
 }
 }  // namespace marlin
