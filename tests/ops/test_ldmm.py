2736e498337f5dd1bd877658c6038118ba162d1f
Xiaozhe Yao <askxzyao@gmail.com>
2024-11-08 11:57:38 +0100

    update test scripts

--- tests/ops/test_ldmm.py
+++ tests/ops/test_ldmm.py
@@ -10,10 +10,6 @@ from triteia.python.ops.utils.generator import generate_model_distribution
 from triteia.python.ops import gen_batched_lora_16_bit
 from triteia.python.ops import gen_batched_sparse_quant4_NT
 
-# for testing
-from triteia.python.capi import add_lora_sgmv_cutlass
-
-
 class TestLORAOp(unittest.TestCase):
     @torch.inference_mode()
     def run_problem(
@@ -36,7 +32,7 @@ class TestLORAOp(unittest.TestCase):
             # n = matrix col size
             # rank = rank
             print(
-                f"Running sbmm problem with nr={nr}, nm_lora={nm_lora}, nm_sbmm={nm_sbmm}, m={m}, n={n}, rank={rank}, distribution={distribution}"
+                f"Running ldmm problem with nr={nr}, nm_lora={nm_lora}, nm_lowprec={nm_sbmm}, m={m}, n={n}, rank={rank}, distribution={distribution}"
             )
             nr_lora = nr // 2
             nr_sbmm = nr - nr_lora
@@ -70,24 +66,22 @@ class TestLORAOp(unittest.TestCase):
             indices_sbmm[indices_sbmm != -1] += nm_lora
             indices = torch.cat((indices_lora, indices_sbmm), 0)
             ldmm_output = ldmm(indices, x, LwA, LwB, qweight, meta, scale, base_weight=None)
-            
+
             # Tolerances from punica
             rtol, atol = (5e-3, 5e-3)
             all_close = torch.allclose(native_output, ldmm_output, rtol = rtol, atol = atol)
-            print(f"The bgvm_output and native_output {'ARE' if all_close else 'ARE NOT'} close.")
-
+            self.assertTrue(all_close)
+            if not all_close:
             # Check which individual elements are close
-            mask = torch.isclose(native_output, ldmm_output, rtol = rtol, atol = atol)
-            num = (~mask).sum().item()
-            print(f"Number of elements that are not close: {num}")
-
-            # Print the differences for elements that are not close
-            for i in range(min(mask.shape[0], 100)):
-                for j in range(min(mask.shape[1], 100)):
-                    if not mask[i, j]:
-                        diff = native_output[i, j] - ldmm_output[i, j]
-                        print(f"Index ({i}, {j}): native_output = {native_output[i, j]}, ldmm_output = {ldmm_output[i, j]}, difference = {diff}")
-
+                mask = torch.isclose(native_output, ldmm_output, rtol = rtol, atol = atol)
+                num = (~mask).sum().item()
+                print(f"Number of elements that are not close: {num}")
+                # Print the differences for elements that are not close
+                for i in range(min(mask.shape[0], 100)):
+                    for j in range(min(mask.shape[1], 100)):
+                        if not mask[i, j]:
+                            diff = native_output[i, j] - ldmm_output[i, j]
+                            print(f"Index ({i}, {j}): native_output = {native_output[i, j]}, ldmm_output = {ldmm_output[i, j]}, difference = {diff}")
         except torch.cuda.OutOfMemoryError as e:
             print(f"Out of memory, skipping nr={nr}, nm_lora={nm_lora}, nm_sbmm={nm_sbmm}, m={m}, n={n}, rank={rank}")
         finally:
@@ -95,23 +89,30 @@ class TestLORAOp(unittest.TestCase):
 
     def test_tiny(self):
         # the rank needs to be divisble by 8
-        rank = 16
-        self.run_problem("uniform",  10, 5, 5, 768, 768, rank)
-        self.run_problem("zipf:1.5", 128, 2, 2, 4096, 12288, rank)
+        ranks = [8,16,32,64]
+        for rank in ranks:
+            self.run_problem("uniform",  10, 5, 5, 768, 768, rank)
+            self.run_problem("zipf:1.5", 128, 2, 2, 4096, 12288, rank)
+            self.run_problem("zipf:1.5", 128, 2, 3, 4096, 12288, rank)
+            self.run_problem("zipf:1.5", 128, 3, 2, 4096, 12288, rank)
+            self.run_problem("zipf:1.5", 128, 16, 16, 4096, 12288, rank)
 
-    # def test_llama(self):
-    #     nrs = [16, 32, 64, 128, 256]
-    #     nms = [[2,4,8,16], [2,4,8,16,32], [2,4,8,16,32,64], [2,4,8,16,32,64,128], [2,4,8,16,32,64,128,256]]
-    #     distributions = ["uniform", "zipf:1.5"]
-    #     for _, layers in llama_shapes.items():
-    #         for layer in layers:
-    #             for nr_id, nr in enumerate(nrs):
-    #                 for nm_id, nm in enumerate(nms[nr_id]):
-    #                     for distribution in distributions:
-    #                         self.run_problem(distribution, nr, nm, layer[0], layer[1])
+
+    def test_llama(self):
+        ranks = [8, 16, 32, 64]
+        nrs = [16, 32, 64, 128, 256]
+        nms_lora = [[2,4,8], [2,4,8,16], [2,4,8,16,32], [2,4,8,16,32,64], [2,4,8,16,32,64,128]]
+        nms_delta = [[2,4,8], [2,4,8,16], [2,4,8,16,32], [2,4,8,16,32,64], [2,4,8,16,32,64,128]]
+        distributions = ["uniform", "zipf:1.5", "zipf:2.0"]
+        for _, layers in llama_shapes.items():
+            for layer in layers:
+                for nr_id, nr in enumerate(nrs):
+                    for nm_id, nm_lora in enumerate(nms_lora[nr_id]):
+                        for nmd_id, nm_delta in enumerate(nms_delta[nr_id]):
+                            for distribution in distributions:
+                                for rank in ranks:
+                                    self.run_problem(distribution, nr, nm_lora, nm_delta, layer[0], layer[1], rank)
 
 
 if __name__ == "__main__":
-    print(f'available devices: {torch.cuda.device_count()}')
-    print(f'current device: { torch.cuda.current_device()}')
     unittest.main()
