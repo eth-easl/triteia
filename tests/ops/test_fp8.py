61a4e66c4b1a2eb68d019c7bafb53db194ce0c0d
Xiaozhe Yao <askxzyao@gmail.com>
2024-11-12 14:37:43 +0100

    finish reorg

--- tests/ops/test_fp8.py
+++ tests/ops/test_fp8.py
@@ -1,24 +1,23 @@
+import pytest
 import torch
-import unittest
 from triteia.python.ops import to_float8, fp8_mm
 import torch.nn.functional as F
 
+from triteia.python.utils.gpu import is_hopper
 
-class TestFP8(unittest.TestCase):
-    def run_problem(self, M, N, K, dtype=torch.float8_e4m3fn):
-        x = torch.randn((1, M, N), dtype=torch.float16, device="cuda")
-        w = torch.randn((N, K), dtype=torch.float16, device="cuda").t()
+
+@pytest.mark.parametrize("m", [512])
+@pytest.mark.parametrize("n", [512])
+@pytest.mark.parametrize("k", [512])
+def test_fp8_mm(m, n, k, dtype=torch.float8_e4m3fn):
+    if not is_hopper():
+        pytest.skip("Test is only for Hopper")
+    else:
+        x = torch.randn((1, m, n), dtype=torch.float16, device="cuda")
+        w = torch.randn((n, k), dtype=torch.float16, device="cuda").t()
         x_f8, x_inv_s = to_float8(x, dtype=dtype)
         w_f8, w_inv_s = to_float8(w)
         y_fp8 = fp8_mm(x, w_f8, out_dtype=torch.float16, scale_b=w_inv_s)
         y_fp16 = torch.matmul(x, w)
-        print(f"y_fp8: {y_fp8.shape}, y_fp16: {y_fp16.shape}")
         cos_sim = F.cosine_similarity(y_fp8.reshape(-1), y_fp16.reshape(-1), dim=0)
-        return cos_sim
-
-    def test_tiny(self):
-        self.run_problem(16, 16, 16, dtype=torch.float8_e4m3fn)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert cos_sim > 0.95
--- tests/ops/test_sdpa.py
+++ tests/ops/test_sdpa.py
@@ -1,24 +1,24 @@
-# import torch
-# import unittest
-# from triteia.python.ops import sdpa
+import torch
+import unittest
+from triteia.python.ops import sdpa
 
 
-# class TestSDPAOp(unittest.TestCase):
-#     def run_problem(self, q, k, v, is_causal, impl):
-#         return sdpa(q, k, v, is_causal=is_causal, impl=impl)
+class TestSDPAOp(unittest.TestCase):
+    def run_problem(self, q, k, v, is_causal, impl):
+        return sdpa(q, k, v, is_causal=is_causal, impl=impl)
 
-#     def test_tiny(self):
-#         bsz = 1
-#         seq_len = 16
-#         num_heads = 4
-#         head_dim = 4
-#         q = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
-#         k = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
-#         v = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
-#         output = self.run_problem(q, k, v, is_causal=True, impl="fa")
-#         ref = self.run_problem(q, k, v, is_causal=True, impl="torch")
-#         self.assertTrue(torch.allclose(output, ref))
+    def test_tiny(self):
+        bsz = 1
+        seq_len = 16
+        num_heads = 4
+        head_dim = 4
+        q = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
+        k = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
+        v = torch.randn((bsz, seq_len, num_heads, head_dim), dtype=torch.float16).cuda()
+        output = self.run_problem(q, k, v, is_causal=True, impl="fa")
+        ref = self.run_problem(q, k, v, is_causal=True, impl="torch")
+        self.assertTrue(torch.allclose(output, ref))
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
